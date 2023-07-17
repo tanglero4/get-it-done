d3b93736845573afda1054695dc47eb27a5ef026
tanglero4 <tiffanyanglero94@hotmail.com>
2023-07-17 18:57:02 -0400

    Added elements to createTaskText function to add a checkbox, span element and delete button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const addTaskBtn = document.getElementById('task-btn');
 const toDoList = document.getElementById('toDo-list');
 
 const addTask = ()=>{
-    const userInput = toDoInput.ariaValueMax.trim();
+    const userInput = toDoInput.value.trim();
 
     if (userInput !==''){
         const taskText = createTaskText(userInput)
@@ -13,4 +13,16 @@ const addTask = ()=>{
 const createTaskText = (userInput)=>{
     const taskText = document.createElement('li');
     taskText.className= "toDo-item";    
+
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.classList.add('checkbox');
+
+    const userInputSpan = document.createElement('span');
+    userInputSpan.textContent=userInput;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    document.classList.add('delete');
+    deleteBtn.addEventListener('click', deleteInput);
 }
